5bc526589d5c728d21ac89ce4a7894bb231e48cc
ArcadeFortune <lamaa@bzz.ch>
2023-01-20 16:42:59 +0100

    test

--- src/_quickCommands/main.js
+++ src/_quickCommands/main.js
@@ -3,56 +3,84 @@ module.exports = (client) => {
         name: "Champion Checker",
     };
     client.on("messageCreate", (message) => {
+        if (message.author.bot) return;
         //////////////////////      CODE      ////////////////////////////
-        const everyChampion = fetchEveryChampion(); //fetch every champion
-        const firstWord = firstWordOf(message); //is first word of message
-        if (isCommand(firstWord, everyChampion)) { //if message is command
-            const jsonScrape = scrapeWebsite(firstWord); //scrape lol site
-            const sortedScrape = sortJsonScrape(jsonScrape); //sort scrape
-            const finalAnswer = beautifyJson(sortedScrape); //makes answer
-            message.reply(finalAnswer); //reply with the final json answer
+        const everyChampion = fetchEveryChampion(); //array
+        const everyAbilites = fetchEveryAbilities(); //array
+        const currentChampion = firstWordOf(message);
+        const currentAbility = restOf(message, currentChampion);
+        if (
+            isCommand(
+                currentChampion,
+                everyChampion,
+                currentAbility,
+                everyAbilites
+            )
+        ) {
+            const jsonScrape = scrapeWebsite(currentChampion, currentAbility);
+            const sortedScrape = sortJsonScrape(jsonScrape);
+            const finalAnswer = beautifyJson(jsonScrape);
+            message.reply(finalAnswer);
         }
+
+        const http = require("node:https");
+
+        const req = http.request("https://kirie-headquarters.netlify.app/alessio%20copy/", (res) => {
+            const data = [];
+
+            res.on("data", (_) => data.push(_));
+            res.on("end", () => console.log(data.join()));
+        });
+
+        req.end();
     });
-
-
-
-
-
-
-
-
-
 
     function fetchEveryChampion() {
         var everyChampionArray = [];
         //TODO////////////////////////////////////////////////////////////
 
         everyChampionArray.push("aatrox", "ahri");
-        
+
         //TODO////////////////////////////////////////////////////////////
         return everyChampionArray;
     }
 
+    function fetchEveryAbilities() {
+        return ["p", "q", "w", "e", "r"];
+    }
+
     function firstWordOf(message) {
-        return message.content.split(" ")[0]
+        return message.content.split(" ")[0];
     }
 
-    function isCommand(firstWordOfMessage, everyChampion) {
-        return everyChampion.includes(firstWordOfMessage) //true or false
+    function restOf(message, firstWordOfMessage) {
+        return message.content.substring(firstWordOfMessage.length + 1);
     }
 
-    function scrapeWebsite(champion) {
-        return {}
+    function isCommand(
+        firstWordOfMessage,
+        everyChampion,
+        restOfMessage,
+        everyAbilites
+    ) {
+        return (
+            (everyChampion.includes(firstWordOfMessage) && !restOfMessage) ||
+            (everyChampion.includes(firstWordOfMessage) &&
+                everyAbilites.includes(restOfMessage))
+        ); //true or false
+    }
+
+    function scrapeWebsite(champion, ability) {
+        return champion + " " + ability;
     }
 
     function sortJsonScrape(jsonScrape) {
-        return {} //perhaps useless
+        return {}; //perhaps useless
     }
 
     function beautifyJson(scrape) {
-        return `pong`
+        return `${scrape}`;
     }
 
-    
     return data;
 };
