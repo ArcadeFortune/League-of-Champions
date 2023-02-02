9d2064590ebbf4102a48df86eef062339a891464
nxtdxve <zettlerd@bzz.ch>
2023-02-02 22:36:14 +0100

    fixxed nunu and all with spacebar

--- src/_quickCommands/main.js
+++ src/_quickCommands/main.js
@@ -39,17 +39,25 @@ module.exports = (client) => {
     }
 
     function firstWordOf(message, everyChampion) {
-        var everyChampionDoubleName = [] //list of all champions with more than one name
+        var everyChampionDoubleName = []; //list of all champions with more than one name
         for (var champName of everyChampion) {
-            if (champName.includes(" ")) {
-                everyChampionDoubleName.push(champName)
-            }
+          if (champName.includes(" ")) {
+            everyChampionDoubleName.push(champName);
+          }
         }
-        return message.content.split(" ")[0].toLowerCase();
-    }
+        let firstWord = message.content.split(" ")[0].toLowerCase();
+        for (var doubleName of everyChampionDoubleName) {
+          if (doubleName.startsWith(firstWord)) {
+            return doubleName;
+          }
+        }
+        return firstWord;
+      }
+      
 
     function restOf(message, firstWordOfMessage) {
-        return message.content.substring(firstWordOfMessage.length + 1).toLowerCase();
+        /* return message.content.substring(firstWordOfMessage.length + 1).toLowerCase(); */
+        return message.content.split(" ").pop().toLowerCase();
     }
 
     function isCommand(
@@ -67,7 +75,15 @@ module.exports = (client) => {
     }
 
     async function scrapeWebsite(champion, ability) {
-        const url = `https://leagueoflegends.fandom.com/wiki/${champion}/LoL`;
+        let url;
+        if (champion === "nunu & willump") {
+            url = "https://leagueoflegends.fandom.com/wiki/Nunu/LoL";
+        } else if (champion === "Jarvan IV") {
+            url = "https://leagueoflegends.fandom.com/wiki/Jarvan_IV/LoL";
+        } else {
+            url = `https://leagueoflegends.fandom.com/wiki/${champion}/LoL`;
+        }
+
         const search = `#mw-content-text > div.mw-parser-output > div.skill.skill_${ability} > div > div > div.champion-ability__header > aside > section`;
 
         let res = await axios.get(url);
@@ -106,11 +122,15 @@ function fetchAllChampions() {
             everyChampionFetched = []; //reset the list
             championSpans.each(function () {
                 everyChampionFetched.push($(this).text().toLowerCase());
+
+            // log every champion with a space in their name or a special character
+            if ($(this).text().includes(" ") || $(this).text().includes("'")) {
+                console.log($(this).text());
+            }
             });
         });
     });
 }
+
 // TODO
-
-// WHEN TYPING 'TAHM', SHOULD RETURN TAHM KNENCH. (in function firstWordOf())
-
+// broken champs: everyone with more than one ability and jarvan iv
